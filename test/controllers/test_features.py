9389b41225719895a9cb6d2bfc9d4e1b17bcd7e8
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2018-11-07 20:22:14 +0100

    fixed feature IDs
    closes #2

--- test/controllers/test_features.py
+++ test/controllers/test_features.py
@@ -10,12 +10,12 @@ class FeaturesControllerTest(unittest.TestCase):
     def test_find_features_all(self):
         ctx = new_test_service_context()
         feature_collection = find_features(ctx, "all")
-        self._assertFeatureCollection(feature_collection, 6, {'1', '2', '3', '4', '5', '6'})
+        self._assertFeatureCollection(feature_collection, 6, {'0', '1', '2', '3', '4', '5'})
 
     def test_find_features_by_box(self):
         ctx = new_test_service_context()
         feature_collection = find_features(ctx, "all", box_coords="-1,49,2,55")
-        self._assertFeatureCollection(feature_collection, 2, {'1', '4'})
+        self._assertFeatureCollection(feature_collection, 2, {'0', '3'})
 
         with self.assertRaises(ServiceBadRequestError) as cm:
             find_features(ctx, "all", box_coords="-1,49,55")
@@ -28,7 +28,7 @@ class FeaturesControllerTest(unittest.TestCase):
     def test_find_features_by_wkt(self):
         ctx = new_test_service_context()
         feature_collection = find_features(ctx, "all", geom_wkt="POLYGON ((2 49, 2 55, -1 55, -1 49, 2 49))")
-        self._assertFeatureCollection(feature_collection, 2, {'1', '4'})
+        self._assertFeatureCollection(feature_collection, 2, {'0', '3'})
 
         with self.assertRaises(ServiceBadRequestError) as cm:
             find_features(ctx, "all", geom_wkt="POLYGLON ((2 49, 2 55, -1 55, -1 49, 2 49))")
@@ -40,15 +40,15 @@ class FeaturesControllerTest(unittest.TestCase):
         geojson_obj = {'type': 'Polygon',
                        'coordinates': (((2.0, 49.0), (2.0, 55.0), (-1.0, 55.0), (-1.0, 49.0), (2.0, 49.0)),)}
         feature_collection = find_features(ctx, "all", geojson_obj=geojson_obj)
-        self._assertFeatureCollection(feature_collection, 2, {'1', '4'})
+        self._assertFeatureCollection(feature_collection, 2, {'0', '3'})
 
         geojson_obj = {'type': 'Feature', 'properties': {}, 'geometry': geojson_obj}
         feature_collection = find_features(ctx, "all", geojson_obj=geojson_obj)
-        self._assertFeatureCollection(feature_collection, 2, {'1', '4'})
+        self._assertFeatureCollection(feature_collection, 2, {'0', '3'})
 
         geojson_obj = {'type': 'FeatureCollection', 'features': [geojson_obj]}
         feature_collection = find_features(ctx, "all", geojson_obj=geojson_obj)
-        self._assertFeatureCollection(feature_collection, 2, {'1', '4'})
+        self._assertFeatureCollection(feature_collection, 2, {'0', '3'})
 
         with self.assertRaises(ServiceBadRequestError) as cm:
             geojson_obj = {'type': 'FeatureCollection', 'features': []}
@@ -58,7 +58,7 @@ class FeaturesControllerTest(unittest.TestCase):
     def test_find_dataset_features(self):
         ctx = new_test_service_context()
         feature_collection = find_dataset_features(ctx, "all", "demo")
-        self._assertFeatureCollection(feature_collection, 3, {'1', '2', '3'})
+        self._assertFeatureCollection(feature_collection, 3, {'0', '1', '2'})
 
     def _assertFeatureCollection(self, feature_collection, expected_count, expected_ids):
         self.assertIsInstance(feature_collection, dict)
@@ -68,5 +68,5 @@ class FeaturesControllerTest(unittest.TestCase):
         features = feature_collection["features"]
         self.assertIsInstance(features, list)
         self.assertEqual(expected_count, len(features))
-        actual_ids = {f["properties"]["ID"] or f["properties"]["id"] for f in features}
+        actual_ids = {f["id"] for f in features if "id" in f}
         self.assertEqual(expected_ids, actual_ids)
